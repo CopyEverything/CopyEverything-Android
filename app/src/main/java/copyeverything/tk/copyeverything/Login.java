9fcdbc76a50a732d5a791b794b216bd4cd191a16
eyesniper2 <eyesniper2@rayfall.net>
2016-05-22 15:23:55 -0600

    Cleaned out permissions for Android 6.0

--- app/src/main/java/copyeverything/tk/copyeverything/Login.java
+++ app/src/main/java/copyeverything/tk/copyeverything/Login.java
@@ -222,31 +222,12 @@ public class Login extends AppCompatActivity implements LoaderCallbacks<Cursor> 
 
     @Override
     public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        return new CursorLoader(this,
-                // Retrieve data rows for the device user's 'profile' contact.
-                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
-                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
-
-                // Select only email addresses.
-                ContactsContract.Contacts.Data.MIMETYPE +
-                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
-                .CONTENT_ITEM_TYPE},
-
-                // Show primary email addresses first. Note that there won't be
-                // a primary email address if the user hasn't specified one.
-                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
+        return null;
     }
 
     @Override
     public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-        List<String> emails = new ArrayList<String>();
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast()) {
-            emails.add(cursor.getString(ProfileQuery.ADDRESS));
-            cursor.moveToNext();
-        }
 
-        addEmailsToAutoComplete(emails);
     }
 
     @Override
@@ -254,24 +235,6 @@ public class Login extends AppCompatActivity implements LoaderCallbacks<Cursor> 
 
     }
 
-    private interface ProfileQuery {
-        String[] PROJECTION = {
-                ContactsContract.CommonDataKinds.Email.ADDRESS,
-                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
-        };
 
-        int ADDRESS = 0;
-        int IS_PRIMARY = 1;
-    }
-
-
-    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
-        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<String>(Login.this,
-                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
-
-        mEmailView.setAdapter(adapter);
-    }
 }
 
--- app/src/main/java/copyeverything/tk/copyeverything/StartListeners.java
+++ app/src/main/java/copyeverything/tk/copyeverything/StartListeners.java
@@ -1,12 +1,11 @@
 package copyeverything.tk.copyeverything;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-
-//import com.firebase.client.*;
-//import com.firebase.client.Firebase;
+import android.support.v4.content.ContextCompat;
 
 /**
  * This is for starting the listeners when the app starts up
@@ -14,23 +13,18 @@ import android.content.SharedPreferences;
 public class StartListeners extends BroadcastReceiver {
 
     public void onReceive(Context context, Intent intent){
+
         if(Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())){
 
             //Get login data
-            SharedPreferences sharedPreferences = context.getSharedPreferences(context.getString(R.string.preference_file_key), Context.MODE_PRIVATE);
-            String email = sharedPreferences.getString(context.getString(R.string.saved_email), "");
-            String password = sharedPreferences.getString(context.getString(R.string.saved_pass), "");
+            //SharedPreferences sharedPreferences = context.getSharedPreferences(context.getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+            //String email = sharedPreferences.getString(context.getString(R.string.saved_email), "");
+            //String password = sharedPreferences.getString(context.getString(R.string.saved_pass), "");
 
-            AuthenticationActivity authenticator = new AuthenticationActivity();
+            //AuthenticationActivity authenticator = new AuthenticationActivity();
 
-            authenticator.attemptLogin(email, password);
+            //authenticator.attemptLogin(email, password);
 
-            /*Firebase.setAndroidContext(context);
-            Intent incomingServiceIntent = new Intent(context, IncomingDataListener.class);
-            context.startService(incomingServiceIntent);
-
-            Intent copyServiceIntent = new Intent(context, CopyListener.class);
-            context.startService(copyServiceIntent);*/
         }
     }
 }
